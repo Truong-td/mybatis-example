9b711b8e5446f1fd4c5ca54abb400c824133e375
Truong TranDuy <truongtd@nal.vn>
2020-08-04 18:29:22 +0700

    rename field

--- src/main/java/com/nal/mybatisexample/entity/User.java
+++ src/main/java/com/nal/mybatisexample/entity/User.java
@@ -2,7 +2,7 @@ package com.nal.mybatisexample.entity;
 
 public class User {
     private Integer id;
-    private String name;
+    private String username;
     private String password;
 
     public Integer getId() {
@@ -13,12 +13,12 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
