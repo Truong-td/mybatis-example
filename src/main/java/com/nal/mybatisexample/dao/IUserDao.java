6d79b6a02596f67123f0caab66b039ca4a8e2b5c
Truong TranDuy <truongtd@nal.vn>
2020-08-04 18:31:25 +0700

    add logic using anotation

--- src/main/java/com/nal/mybatisexample/dao/IUserDao.java
+++ src/main/java/com/nal/mybatisexample/dao/IUserDao.java
@@ -2,10 +2,21 @@ package com.nal.mybatisexample.dao;
 
 import com.nal.mybatisexample.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface IUserDao {
-    List<User> getAll();
+    // get all student
+    final String GET_ALL_USER = "SELECT * FROM USER";
+
+    @Select(GET_ALL_USER)
+    @Results(value = { @Result(property = "id", column = "ID"),
+            @Result(property = "username", column = "username"),
+            @Result(property = "password", column = "password")})
+    public List<User> getAll();
+
 }
